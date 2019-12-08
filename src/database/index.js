fc69e22a835e1d88a3017bb171e2a7993f37f626
abauruel <abauruel@gmail.com>
2019-12-08 09:20:47 -0300

    added new model in configuration

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
+import Registration from '../app/models/Registration';
 
-const models = [User, Student, Plan];
+const models = [User, Student, Plan, Registration];
 
 class Database {
   constructor() {
@@ -14,7 +15,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
