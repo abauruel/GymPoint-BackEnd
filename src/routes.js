fceb57b9e5715c642ccb5d7ae39253301b82ab16
abauruel <abauruel@gmail.com>
2019-12-08 11:23:22 -0300

    added new route for registration

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import RegistrationController from './app/controllers/RegistrationController';
 import Middleware from './app/middlewares/auth';
 
 const route = new Router();
@@ -15,5 +16,9 @@ route.get('/plans', PlanController.index);
 route.post('/plan', PlanController.store);
 route.put('/plan/:id', PlanController.update);
 route.delete('/plan/:id', PlanController.delete);
+route.get('/registrations', RegistrationController.index);
+route.post('/registration', RegistrationController.store);
+route.put('/registration/:id', RegistrationController.update);
+route.delete('/registration/:id', RegistrationController.delete);
 
 export default route;
