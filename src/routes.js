1d95582a61293a8e35603637b6b24d7a9c3b1d43
abauruel <abauruel@gmail.com>
2019-12-08 08:36:04 -0300

    added new route with methods

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 import Middleware from './app/middlewares/auth';
 
 const route = new Router();
@@ -10,5 +11,9 @@ route.post('/session', SessionController.store);
 route.use(Middleware);
 route.post('/student', StudentController.store);
 route.put('/student/:id', StudentController.update);
+route.get('/plans', PlanController.index);
+route.post('/plan', PlanController.store);
+route.put('/plan/:id', PlanController.update);
+route.delete('/plan/:id', PlanController.delete);
 
 export default route;
