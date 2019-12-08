29155f4ef180f721900502da9e52bcde7792eff7
abauruel <abauruel@gmail.com>
2019-12-08 08:30:00 -0300

    added new controller with methods CRUD

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,50 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {
+    const plans = await Plan.findAll();
+    if (!plans) {
+      return res.status(400).json({ error: 'No plan found' });
+    }
+    return res.json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation is failed' });
+    }
+    const plans = await Plan.create(req.body);
+    return res.json(plans);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation is failed' });
+    }
+    const plan = await Plan.findByPk(req.params.id);
+    if (!plan) {
+      return res.status(400).json({ error: 'Invalid plan' });
+    }
+    await plan.update(req.body);
+
+    return res.json(plan);
+  }
+
+  async delete(req, res) {
+    await Plan.findByPk(req.params.id);
+    return res.status(200);
+  }
+}
+
+export default new PlanController();
