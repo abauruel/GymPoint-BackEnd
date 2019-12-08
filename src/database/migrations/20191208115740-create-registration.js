2b6bb78b8f84c335704794ca6319ecc5f79647a9
abauruel <abauruel@gmail.com>
2019-12-08 11:24:47 -0300

    fixed name of migration

--- src/database/migrations/20191208115740-create-registration.js
+++ src/database/migrations/20191208115740-create-registration.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('registration', {
+    return queryInterface.createTable('registrations', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -51,6 +51,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('registration');
+    return queryInterface.dropTable('registrations');
   },
 };
