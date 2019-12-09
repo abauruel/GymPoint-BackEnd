733541942d83d942f75c7ef75619eb2363e62bbe
abauruel <abauruel@gmail.com>
2019-12-09 12:09:42 -0300

    added email config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c9244b917177ac',
+    pass: '3246fb7fc4a6f1',
+  },
+  default: {
+    from: ' Equipe GymPoint <noreply@gympoint.com>',
+  },
+};
