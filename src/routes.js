1c58708cc92e1cd0921be5f93a819ac58280bd54
abauruel <abauruel@gmail.com>
2019-12-09 21:06:16 -0300

    added new routes for Checkin and HelpOrder

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,20 @@ import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
 import RegistrationController from './app/controllers/RegistrationController';
+import CheckinController from './app/controllers/CheckinController';
+import HelpOrderController from './app/controllers/HelpOrderController';
 import Middleware from './app/middlewares/auth';
 
 const route = new Router();
 
 route.post('/session', SessionController.store);
+
+route.post('/students/:id/checkins', CheckinController.store);
+route.get('/students/:id/checkins', CheckinController.index);
+
+route.post('/students/:id/help-orders', HelpOrderController.store);
+route.get('/students/:id/help-orders', HelpOrderController.show);
+
 route.use(Middleware);
 route.post('/student', StudentController.store);
 route.put('/student/:id', StudentController.update);
@@ -20,5 +29,8 @@ route.get('/registrations', RegistrationController.index);
 route.post('/registration', RegistrationController.store);
 route.put('/registration/:id', RegistrationController.update);
 route.delete('/registration/:id', RegistrationController.delete);
+
+route.get('/students/help-orders', HelpOrderController.index);
+route.post('/help-orders/:id/answer', HelpOrderController.update);
 
 export default route;
